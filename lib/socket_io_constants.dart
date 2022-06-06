46da1210f851b890879fb8be45c77feef19af22c
hemang-concetto <hemang.shah@concettolabs.com>
2022-06-06 16:36:01 +0530

    Configuration : Configuration for enum and extension

--- lib/socket_io_constants.dart
+++ lib/socket_io_constants.dart
@@ -3,5 +3,7 @@
 /// More dartdocs go here.
 library socket_io_constants;
 
+export 'src/enum/io_content_type.dart';
+export 'src/extension/enum_extension.dart';
 export 'src/socket_io_constants_base.dart';
 export 'src/socket_io_model_base.dart';
