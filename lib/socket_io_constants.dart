314f18a7220ec2136d6ac6cbb53158be09fd5449
hemang-concetto <hemang.shah@concettolabs.com>
2022-06-08 15:53:55 +0530

    Extension: Updated Extension Functions

--- lib/socket_io_constants.dart
+++ lib/socket_io_constants.dart
@@ -5,5 +5,7 @@ library socket_io_constants;
 
 export 'src/enum/io_content_type.dart';
 export 'src/extension/enum_extension.dart';
+export 'src/extension/date_time_extension.dart';
+export 'src/extension/string_extension.dart';
 export 'src/socket_io_constants_base.dart';
 export 'src/socket_io_model_base.dart';
